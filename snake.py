f1be42a33ac46d5b3df8432f6db2fc042a1a185f
mari <souzadeaguiar.ma@gmail.com>
2026-06-23 20:10:21 -0300

    tela/janela feita

--- snake.py
+++ snake.py
@@ -1,1 +1,17 @@
 from pygame import *
+
+init()
+def tela():
+    display.set_mode((800,800))
+    display.set_caption("Snake")
+    rodando = True
+    while rodando: 
+        for eventos in event.get():
+            if eventos.type == QUIT:
+                rodando = False
+        
+        display.flip()
+    
+
+tela()
+quit()
