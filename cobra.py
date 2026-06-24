215a5d4acfa49b601df408e99ef73d2cb2961d67
mari <souzadeaguiar.ma@gmail.com>
2026-06-24 17:54:46 -0300

    rayquaza imagens

--- cobra.py
+++ cobra.py
@@ -7,8 +7,14 @@ class Cobra():
     def __init__(self):
         self.rayquaza = image.load("rayquaza.png")
         self.rayquaza = transform.scale(self.rayquaza, (100, 100))
-        self.rayquaza_direita = image.load("rayquaza.png")
-        self.rayquaza_direita = transform.scale(self.rayquaza, (100, 100))
+        self.rayquaza_direita = image.load("rayquazadireita.png")
+        self.rayquaza_direita = transform.scale(self.rayquaza_direita, (100, 100))
+        self.rayquaza_cima = image.load("rayquaza_cima.png")
+        self.rayquaza_cima = transform.scale(self.rayquaza_cima, (100, 100))
+        self.rayquaza_esquerda = image.load("rayquaza_esquerda.png")
+        self.rayquaza_esquerda = transform.scale(self.rayquaza_esquerda, (100, 100))
+        self.rayquaza_parado = image.load("rayquaza_parado.png")
+        self.rayquaza_parado = transform.scale(self.rayquaza_parado, (100, 100))
         self.cobra = [[200, 200], [240, 200]]
         self.skin = Surface((20,20))
         self.skin.fill((0, 250, 55))
