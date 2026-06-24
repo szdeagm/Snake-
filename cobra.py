e59e2b7a2ae881c2858334239d792057ed0326e3
mari <souzadeaguiar.ma@gmail.com>
2026-06-24 11:57:17 -0300

    movimentação parcialmente funcionando, corpo some e fruta não aparece

--- cobra.py
+++ cobra.py
@@ -8,4 +8,13 @@ class Cobra():
         self.skin.fill((0, 250, 55))
         self.direction = "Right"
         self.change_to = self.direction
-        self.cobra_direction = [100, 50]
+        self.cobra_direction = [240, 200]
+
+class Fruta():
+    def __init__(self):
+        x = 300
+        y = 300
+        self.fruta = [x , y]
+        self.skin = Surface((10,10))
+        self.skin.fill((40, 250, 55))
+        self.fruta_spaw = True
--- snake.py
+++ snake.py
@@ -1,16 +1,20 @@
 from pygame import *
 from pygame.locals import *
 from cobra import *
-from time import *
+from pygame import time
 from random import *
 
 init()
 
 cobra = Cobra()
+fruta = Fruta()
 tela = display.set_mode((600,600))
 display.set_caption("Snake")
+fps = time.Clock()
 rodando = True
+
 while rodando: 
+    fps.tick(10)
     for eventos in event.get():
         if eventos.type == QUIT:
             rodando = False
@@ -23,18 +27,32 @@ while rodando:
                 cobra.change_to = "LEFT"
             if eventos.key == K_RIGHT:
                 cobra.change_to = "RIGHT"
+        
+    if cobra.change_to == 'UP' and cobra.direction != 'DOWN':
+        cobra.direction = 'UP'
+    if cobra.change_to == 'DOWN' and cobra.direction != 'UP':
+        cobra.direction = 'DOWN'
+    if cobra.change_to == 'LEFT' and cobra.direction != 'RIGHT':
+        cobra.direction = 'LEFT'
+    if cobra.change_to == 'RIGHT' and cobra.direction != 'LEFT':
+        cobra.direction = 'RIGHT'
 
-    for cobra_pos in cobra.cobra:
-        tela.blit(cobra.skin, cobra_pos)  
+    for i in range(len(cobra.cobra) - 1):
+        cobra.cobra[i] = cobra.cobra[i + 1]
+
     if cobra.direction == "UP":
-        cobra.cobra_direction[1] -= 10
+        cobra.cobra[-1][1] -= 10
     if cobra.direction == "DOWN":
-        cobra.cobra_direction[1] += 10
+        cobra.cobra[-1][1] += 10
     if cobra.direction == "LEFT":
-        cobra.cobra_direction[0] -= 10
+        cobra.cobra[-1][0] -= 10
     if cobra.direction == "RIGHT":
-        cobra.cobra_direction[0] += 10
-  
+        cobra.cobra[-1][0] += 10
+
+    tela.fill((0,0,0))
+    for cobra_pos in cobra.cobra:
+        tela.blit(cobra.skin, cobra_pos)
+    
     display.update()
 
 quit()
