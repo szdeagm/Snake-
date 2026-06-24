79040728807df67c9ef15badc6bd674434a15b0a
mari <souzadeaguiar.ma@gmail.com>
2026-06-23 22:10:04 -0300

    movimentação não funciona, não sei o q fazer :(

--- cobra.py
+++ cobra.py
@@ -3,6 +3,9 @@ from pygame.locals import *
 
 class Cobra():
     def __init__(self):
-        self.cobra = [(200, 200), (210, 200), (220, 200), (230, 200), (240, 200)]
+        self.cobra = [[200, 200], [210, 200], [220, 200], [230, 200], [240, 200]]
         self.skin = Surface((10,10))
         self.skin.fill((0, 250, 55))
+        self.direction = "Right"
+        self.change_to = self.direction
+        self.cobra_direction = [100, 50]
--- snake.py
+++ snake.py
@@ -1,6 +1,8 @@
 from pygame import *
 from pygame.locals import *
 from cobra import *
+from time import *
+from random import *
 
 init()
 
@@ -12,9 +14,27 @@ while rodando:
     for eventos in event.get():
         if eventos.type == QUIT:
             rodando = False
+        elif eventos.type == KEYDOWN:
+            if eventos.key == K_UP:
+                cobra.change_to = "UP"
+            if eventos.key == K_DOWN:
+                cobra.change_to = "DOWN"
+            if eventos.key == K_LEFT:
+                cobra.change_to = "LEFT"
+            if eventos.key == K_RIGHT:
+                cobra.change_to = "RIGHT"
 
     for cobra_pos in cobra.cobra:
-        tela.blit(cobra.skin, cobra_pos)    
-    display.flip()
+        tela.blit(cobra.skin, cobra_pos)  
+    if cobra.direction == "UP":
+        cobra.cobra_direction[1] -= 10
+    if cobra.direction == "DOWN":
+        cobra.cobra_direction[1] += 10
+    if cobra.direction == "LEFT":
+        cobra.cobra_direction[0] -= 10
+    if cobra.direction == "RIGHT":
+        cobra.cobra_direction[0] += 10
+  
+    display.update()
 
 quit()
